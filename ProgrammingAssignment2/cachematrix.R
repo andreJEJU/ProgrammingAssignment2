fb6af0fea9a1e38ac7e1ef6a911f9bcc2329d9fd
andreJEJU <andrejeju@gmail.com>
2016-11-04 14:58:49 +0100

    Revert "Cache matrix inverse V2"
    This reverts commit 7e80df1b8c896d4997d6a3a1a19a0497dd06d88e.

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -1,46 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function create a special matrix object that can cache its inverse
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
   matrixInverse <- NULL
-
-  set <- function(y) {
-    x <<- y
-    matrixInverse <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(inverse) matrixInverse <<- inverse 
-  getInverse <- function() matrixInverse
-  list(set = set, get = get,
-       setInverse = setInverse,
-       getInverse = getInverse)
+  matrixInverse <- cacheSolve()
+  matrixInverse
   
 }
 
 
-## This function return a matrix that is inverse of 'x'
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  
-        matrixInverse <- x$getinverse()
-    
-        if(!is.null(matrixInverse)) {
-              message("getting cached matrix inverse")
-              return(matrixInverse)
-         }
-  
-        matrixInverse <- x$get()
-  
-        dimension <- dim(matrixInverse)
-  
-        if(dimension[1]==dimension[2]){
-              matrixInverse <- solve(matrixInverse)
-              return(matrixInverse)
-        }else{
-          message("This matrix is not square so the inverse can't be calculated")
-        }
-  
+  if(!is.null(x)) {
+    matrixInverse <- solve(x)
+  }
 }
